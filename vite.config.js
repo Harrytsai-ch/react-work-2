6ce364032c77d9290cdd3938ac0f1393d4bd2a64
HarryTsai <sleepingturtle990866@gmail.com>
2025-01-16 13:14:35 +0800

    update path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/react-gh-pages-sample/':'/',
+  //前者改成github的庫
+  base: process.env.NODE_ENV === 'production' ? '/react-work-2/':'/',
   plugins: [react()],
 })
